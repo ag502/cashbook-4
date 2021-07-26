e6bc03648db0c3b1881e19c3f3141dbf597f67e9
LeeJaeyun <ag502@naver.com>
2021-07-26 21:00:47 +0900

    etc : front end webpack 설정

---
+++ frontend/webpack.config.dev.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const BUILD_DIR = path.resolve(__dirname, "dist")
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+    },
+  },
+  entry: "./src/index.js",
+  output: {
+    path: BUILD_DIR,
+    filename: "bundle.js",
+    assetModuleFilename: 'assets/images/[name][ext]'
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: [
+          {loader: 'babel-loader', options: {
+            presets: [['@babel/preset-env']]
+          }}
+        ]
+      },
+      {
+        test: /.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /.(png|jpg|jpeg|svg)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  }),
+  new MiniCssExtractPlugin({
+    filename: 'index.css'
+  })
+],
+  devServer: {
+    contentBase: BUILD_DIR
+  }
+}
