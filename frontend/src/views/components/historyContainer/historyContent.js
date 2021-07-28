12ea24d2fac91a9166c2dd34841655b8ee1968ec
secretjuju <dev.tmdqh@gmail.com>
2021-07-28 20:12:03 +0900

    add : record List 및 record를 historyContent에 작성

--- frontend/src/views/components/historyContainer/historyContent.js
+++ frontend/src/views/components/historyContainer/historyContent.js
@@ -12,7 +12,47 @@ class HistoryContent extends HTMLElement {
 
   render = () => {
     this.innerHTML = /*html*/ `
-      
+
+        <div class="day-records">
+            <div class="info">
+                <div class="left">
+                    <span class="date">
+                        7월 15일
+                    </span>
+                    <span class="day">
+                        목
+                    </span>
+                </div>
+
+                <div class="right">
+                    <span>수입 1,822,480</span>
+                    <span>지출 9,500</span>
+                </div>
+            </div>
+
+            <div class="record-list">
+                <div class="record"> 
+                    <div class="left">
+                        <div class="category category1">
+                            생활
+                        </div>
+
+                        <p class="context">
+                            7월 월세
+                        </p>
+                    </div>
+                    <div class="right">
+                        <div class="payment">
+                            현대카드
+                        </div>
+                        <div class="price">
+                            -500,000 원
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     `;
   };
 }
