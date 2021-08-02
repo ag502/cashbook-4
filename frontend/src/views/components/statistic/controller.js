d1535255ed9b095557f1bb4e9362cdca6416c9ff
LeeJaeyun <ag502@naver.com>
2021-08-03 01:09:11 +0900

    feature : chart controller 구현

---
+++ frontend/src/views/components/statistic/controller.js
@@ -0,0 +1,54 @@
+import BaseController from '@/common/utils/baseController';
+
+class ChartController extends BaseController {
+  constructor() {
+    super();
+  }
+
+  _getExpenditurePercentage = (total, accountsInfo) => {
+    Object.keys(accountsInfo).forEach((categoryId) => {
+      const curPrice = accountsInfo[categoryId].price;
+      accountsInfo[categoryId].percentage = (curPrice / total) * 100;
+    });
+
+    const diff =
+      100 -
+      Object.entries(accountsInfo).reduce(
+        (acc, [_, { percentage }]) => acc + Math.floor(percentage),
+        0
+      );
+
+    return Object.entries(accountsInfo)
+      .sort((a, b) => {
+        const aDecimal = a[1].percentage - Math.floor(a[1].percentage);
+        const bDecimal = b[1].percentage - Math.floor(b[1].percentage);
+        return bDecimal - aDecimal;
+      })
+      .map(([categoryId, { price, percentage }], idx) => {
+        return {
+          categoryId,
+          price,
+          percent:
+            idx < diff ? Math.floor(percentage) + 1 : Math.floor(percentage),
+        };
+      });
+  };
+
+  getAccountsByCateogroy = () => {
+    const accountsByCategory = {};
+    let totalExpenditure = 0;
+
+    this.cashBookModel.getAccounts().forEach(({ category, price }) => {
+      if (price < 0) {
+        totalExpenditure += -price;
+        accountsByCategory[category] = accountsByCategory[category] || {
+          price: 0,
+        };
+        accountsByCategory[category].price += -price;
+      }
+    });
+    return this._getExpenditurePercentage(totalExpenditure, accountsByCategory);
+  };
+}
+
+export default new ChartController();
