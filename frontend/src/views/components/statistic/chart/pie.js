1504c60a594b044c96cce7b3107194b467815ed0
LeeJaeyun <ag502@naver.com>
2021-08-02 17:09:13 +0900

    feature : pie 차트 구현

---
+++ frontend/src/views/components/statistic/chart/pie.js
@@ -0,0 +1,111 @@
+import { PI, SVG_URL } from '@/common/utils/constant';
+
+const MASK_ID = 'progress';
+
+const convertRadian = (degree) => {
+  return (degree / 180) * PI;
+};
+
+class PieChart extends HTMLElement {
+  constructor() {
+    super();
+    this.initBasicData();
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  initBasicData = () => {
+    this.config = JSON.parse(this.getAttr('config'));
+    this.svgW = parseInt(this.getAttr('width')) || 300;
+    this.svgH = parseInt(this.getAttr('height')) || 300;
+
+    this.style.display = 'block';
+
+    this.radius = this.svgW * 0.25;
+    [this.centerX, this.centerY] = [this.svgW / 2, this.svgH / 2];
+    [this.startX, this.startY] = [this.centerX, this.centerY - this.radius];
+  };
+
+  getFinishCoor = (degree) => {
+    const finishX =
+      this.centerX + this.radius * Math.cos(convertRadian(degree + 270));
+    const finishY =
+      this.centerY + this.radius * Math.sin(convertRadian(degree + 270));
+    return [finishX, finishY];
+  };
+
+  createProgressMask = () => {
+    return document
+      .createElementNS(SVG_URL, 'mask')
+      .setAttr('id', MASK_ID)
+      .setAttr('maskUnits', 'userSpaceOnUse').setHTML(/*html*/ `
+        <rect x='-1' y='-1' height='100%' width='100%' fill='#000000'/>
+        <circle
+          r=${this.radius}
+          cx=${this.centerX}
+          cy=${this.centerY}
+          fill='none'
+          stroke='#ffffff'
+          stroke-width=${this.radius}
+          transform='rotate(-90 ${this.centerX} ${this.centerY})'
+        />
+      `);
+  };
+
+  composePathAttr = (startX, startY, finishX, finishY, isLargeArc) => {
+    return `
+      M ${startX} ${startY}
+      A ${this.radius} ${this.radius}, 0, ${
+      isLargeArc ? 1 : 0
+    }, 1 ${finishX} ${finishY}
+    `;
+  };
+
+  createPartialPie = (pathAttr, color) => {
+    return document
+      .createElementNS(SVG_URL, 'path')
+      .setAttr('d', pathAttr)
+      .setAttr('stroke', color)
+      .setAttr('stroke-width', `${this.radius}`)
+      .setAttr('fill', 'transparent')
+      .setAttr('mask', `url(#${MASK_ID})`);
+  };
+
+  render = () => {
+    const { datasets } = this.config;
+    const $svg = document
+      .createElementNS(SVG_URL, 'svg')
+      .addElement(this.createProgressMask());
+
+    $svg.style.width = `${this.svgW}px`;
+    $svg.style.height = `${this.svgH}px`;
+
+    let accAngle = 0;
+    let [curX, curY] = [this.startX, this.startY];
+    datasets.data.forEach((data, idx) => {
+      const curAngle = (360 * data) / 100;
+      accAngle += curAngle;
+      const [finishX, finishY] = this.getFinishCoor(accAngle);
+
+      const pathAttr = this.composePathAttr(
+        curX,
+        curY,
+        finishX,
+        finishY,
+        curAngle > 180 ? true : false
+      );
+      [curX, curY] = [finishX, finishY];
+
+      $svg.addElement(
+        this.createPartialPie(pathAttr, datasets.backgroundColor[idx])
+      );
+    });
+
+    this.addElement($svg);
+  };
+}
+
+customElements.define('pie-chart', PieChart);
+export default customElements.get('pie-chart');
