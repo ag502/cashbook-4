a7d18a31b3fff7038c63f86568bf628102d9a3ed
secretjuju <dev.tmdqh@gmail.com>
2021-08-02 22:47:30 +0900

    add : http status code util을 만들어서 auth 에 적용하였습니다.

--- backend/src/api/routes/auth.js
+++ backend/src/api/routes/auth.js
@@ -4,7 +4,7 @@ import authService from '../../services/auth.js';
 import githubOauthService from '../../services/github-oauth.js';
 import { errorTypes } from '../../errors/index.js';
 
-import env from '../../config/env.js';
+import STATUS_CODES from '../../utils/http-status.js';
 
 export default (app) => {
   const routes = Router();
@@ -18,39 +18,39 @@ export default (app) => {
     const { nickname, password } = req.body;
     const result = await authService.login({ nickname, password });
     if (result.success) {
-      return res.status(200).json(result);
+      return res.status(STATUS_CODES.OK).json(result);
     }
     if (result.error.errorType === errorTypes.LoginFailed) {
-      return res.status(401).json(result); // unauthorized
+      return res.status(STATUS_CODES.UNAUTHORIZED).json(result); // unauthorized
     }
-    return res.status(500).json(result);
+    return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json(result);
   });
 
   routes.post('/register', async (req, res) => {
     const { nickname, password } = req.body;
     const result = await authService.register({ nickname, password });
     if (result.success) {
-      return res.status(200).json(result); // TODO: Status code 상수 처리 필요
+      return res.status(STATUS_CODES.OK).json(result); // TODO: Status code 상수 처리 필요
     }
     if (result.error.errorType === errorTypes.AlreadyExist) {
-      return res.status(409).json(result); // 409 conflict
+      return res.status(STATUS_CODES.CONFLICT).json(result); // 409 conflict
     }
-    return res.status(500).json(result); // 500 unexpect
+    return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json(result); // 500 unexpect
   });
 
   routes.get('/github', (req, res) => {
     const redirectUrl = githubOauthService.getGithubAuthUrl();
-    res.json(redirectUrl);
+    res.status(STATUS_CODES.OK).json(redirectUrl);
   });
   routes.post('/github-callback', async (req, res) => {
     const { code } = req.body;
     const result = await githubOauthService.githubLogin({ code });
     if (result.success) {
-      return res.status(200).json(result);
+      return res.status(STATUS_CODES.OK).json(result);
     }
     if (result.error.errorType === errorTypes.LoginFailed) {
-      return res.status(401).json(result);
+      return res.status(STATUS_CODES.UNAUTHORIZED).json(result);
     }
-    return res.status(500).json(result);
+    return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json(result);
   });
 };
