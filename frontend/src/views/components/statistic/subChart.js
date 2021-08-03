3e0eea6bef0d498af578e455fa0588ca91ecbee7
LeeJaeyun <ag502@naver.com>
2021-08-03 18:26:08 +0900

    feature : 카테고리 별 달 지출 컴포넌트 및 스타일링 추가

--- frontend/src/views/components/statistic/subChart.js
+++ frontend/src/views/components/statistic/subChart.js
@@ -1,8 +1,15 @@
 import LineChart from '@/views/components/statistic/chart/line';
+import chartController from './controller';
+import observer from '@/common/utils/observer';
+import DayAccount from '@/views/components/dayAccount';
+import notifyTypes from '@/common/utils/notifyTypes';
 
 class SubChart extends HTMLElement {
   constructor() {
     super();
+    this.chartController = chartController;
+    this.observer = observer;
+    this.monthExpByCategory = this.chartController.getMonthExpByCategory(5);
     this.lineData = {
       datasets: {
         data: [20, 100, 1000, 10000, 23453, 53533, 123213, 99999],
@@ -12,10 +19,24 @@ class SubChart extends HTMLElement {
   }
 
   connectedCallback() {
+    this.observer.subscribe(
+      notifyTypes.FETCHED_DATA,
+      this,
+      this.handleFetchedData
+    );
     this.render();
   }
 
+  handleFetchedData = () => {
+    this.monthExpByCategory = this.chartController.getMonthExpByCategory(5);
+    this.render();
+  };
+
   render = () => {
+    const dayAccountKeys = Object.keys(this.monthExpByCategory).sort(
+      (a, b) => b - a
+    );
+
     this.setHTML(/*html*/ `
       <div class='chart--container sub'>
         <line-chart 
@@ -27,6 +48,14 @@ class SubChart extends HTMLElement {
         </line-chart>
       </div>
     `);
+
+    dayAccountKeys.forEach((key) => {
+      const $dayAccount = new DayAccount({
+        date: key,
+        accounts: this.monthExpByCategory[key],
+      });
+      this.appendChild($dayAccount);
+    });
   };
 }
 
