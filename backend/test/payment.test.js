fa9c59fbc5d3f06d6ee24585e851f79b064394cb
secretjuju <dev.tmdqh@gmail.com>
2021-08-04 15:06:04 +0900

    fix : 로그인 안된 사용자 response 안오는것 해결 및 payment 테스트케이스 추가

--- backend/test/payment.test.js
+++ backend/test/payment.test.js
@@ -1,5 +1,6 @@
 import { assert } from 'chai';
 import request from 'request';
+import { errorTypes } from '../src/errors/index.js';
 
 const payment1 = {
   name: '현대카드',
@@ -27,6 +28,30 @@ const paymentTest = ({ BASEURL, userInfo }) =>
                 console.log(body);
               }
               assert(body.success);
+              done();
+            }
+          );
+        });
+        it('로그인 안하고 create payment 테스트', (done) => {
+          request.post(
+            BASEURL + '/api/payment',
+            {
+              body: payment1,
+              json: true,
+            },
+            (err, res, body) => {
+              if (err) {
+                console.log(err);
+              }
+              if (body.success) {
+                assert(false);
+              } else {
+                if (body.error.errorType === errorTypes.UnAuhorized) {
+                  assert(true);
+                } else {
+                  assert(false);
+                }
+              }
               done();
             }
           );
