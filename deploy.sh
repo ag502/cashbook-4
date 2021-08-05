adb1d02e8eb431c5a5ddee5cf251cb4092e5f2a0
Ubuntu <ubuntu@ip-172-31-4-177.ap-northeast-2.compute.internal>
2021-08-05 18:58:54 +0000

    feature : deploy 에서 git commit 감지

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
   
+## github ##
+
+git pull
+
+currentCommit=$(git log -1 --format="%H")
+lastCommit=$(cat ~/.lastCommit.log 2> /dev/null)
+echo $currentCommit > ~/.lastCommit.log
+
+if [ "$currentCommit" == "$lastCommit" ];
+then
+	echo "commit not changed"
+	exit 0
+fi
+
+## 빌드 ##
 cd frontend
 npm install
 npm run build
-cp -r dist/* /var/www/html/
+cp -r dist/* /home/ubuntu/www/
 
-sudo service nginx restart
+cd ../backend
+
+npm install
+npm run deploy
